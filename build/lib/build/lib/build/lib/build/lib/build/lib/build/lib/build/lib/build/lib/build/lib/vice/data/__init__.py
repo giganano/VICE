d7f4a501569f4c5d2d5c92fdc48fb46dc35460a7
astrobeard <astrobeard21@gmail.com>
2018-09-25 01:17:15 -0400

    build removal

--- build/lib/build/lib/build/lib/build/lib/build/lib/build/lib/build/lib/build/lib/build/lib/vice/data/__init__.py
+++
@@ -1,9 +0,0 @@
-
-from __future__ import absolute_import
-
-__all__ = [str("agb_yield_grid"), str("integrated_cc_yield")]
-
-from ._agb_yields import yield_grid as agb_yield_grid
-# from _ccsne_yields.imf_integrator import integrate as integrated_cc_yield
-from ._ccsne_yields import integrated_cc_yield
-
