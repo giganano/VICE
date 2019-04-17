76ae6e94623b987f7f7ef34ad6236b1922360a49
giganano <giganano9@gmail.com>
2019-04-17 15:24:44 -0400

    jw19handling

---
+++ JW19/__main__.py
@@ -0,0 +1,11 @@
+
+from __future__ import print_function
+
+if __name__ == "__main__": 
+	message = "Johnson & Weinberg (2019) has not yet been announced." 
+	message += "Code which produces their plots will be made publicly " 
+	message += "available when the paper is announced."
+	print(message)
+
+
+
