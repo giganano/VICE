361a478832e5036f1641845560a588b67fc0d99e
astrobeard <astrobeard21@gmail.com>
2019-07-23 15:47:52 -0400

    multizone progress

--- vice/core/_pymultizone.pyx
+++ vice/core/_pymultizone.pyx
@@ -21,6 +21,7 @@ from ._output import output
 from ._pysinglezone import _RECOGNIZED_MODES_ 
 from ._pysinglezone import _RECOGNIZED_DTDS_ 
 from ._pysinglezone import singlezone 
+from ._migration_matrix import migration_matrix 
 from ..yields import agb 
 from ..yields import ccsne 
 from ..yields import sneia 
@@ -29,7 +30,6 @@ from . import _pyutils
 import math as m 
 import warnings 
 import numbers 
-import inspect 
 import pickle 
 import sys 
 import os 
@@ -86,6 +86,7 @@ cdef class multizone:
 
 	cdef MULTIZONE *_mz 
 	cdef object _zones 
+	cdef object _migration 
 
 	def __cinit__(self, int n): 
 		if n > 0: 
@@ -98,8 +99,17 @@ cdef class multizone:
 		else: 
 			raise ValueError("Number of zones must be positive. Got: %d" % (n)) 
 
-	def __init__(self, int n, name = "multizonemodel"): 
+	def __init__(self, int n, 
+		name = "multizonemodel", 
+		n_zones = 10, 
+		n_tracers = 1, 
+		verbose = False): 
+		
 		self.name = name 
+		self.migration = migration_specifications(n) 
+		self.n_zones = n_zones 
+		self.n_tracers = n_tracers 
+		self.verbose = verbose 
 
 	def __dealloc__(self): 
 		_multizone.multizone_free(self._mz) 
@@ -154,6 +164,151 @@ empty string.""")
 		else: 
 			raise TypeError("Attribute 'name' must be of type str. Got: %s" % (
 				type(value))) 
+
+	@property 
+	def n_zones(self): 
+		# docstring in python version 
+		return self._mz[0].n_zones 
+
+	@n_zones.setter 
+	def n_zones(self, value): 
+		""" 
+		The number of zones in the simulation 
+
+		Allowed Types 
+		============= 
+		real number 
+
+		Allows Values 
+		============= 
+		Positive integers 
+		""" 
+		if isinstance(value, numbers.Number): 
+			if value > 0: 
+				if value % 1 == 0: 
+					self._mz[0].n_zones = int(value) 
+				else: 
+					raise ValueError("""Attribute 'n_zones' must be \
+interpretable as an integer. Got: %g""" % (value)) 
+			else: 
+				raise ValueError("""Attribute 'n_zones' must be positive. \
+Got: %g""" % (value)) 
+		else: 
+			raise TypeError("""Attribute 'n_zones' must be an integer. \
+Got: %s""" % (type(value))) 
+
+	@property 
+	def n_tracers(self): 
+		# docstring in python version 
+		return self._mz[0].n_tracers 
+
+	@n_tracers.setter 
+	def n_tracers(self, value): 
+		""" 
+		The number of tracer particles per zone per timestep 
+
+		Allowed Types 
+		============= 
+		real number 
+
+		Allowed Values 
+		============== 
+		Positive integers 
+		""" 
+		if isinstance(value, numbers.Number): 
+			if value > 0: 
+				if value % 1 == 0: 
+					self._mz[0].n_tracers = int(value) 
+				else: 
+					raise ValueError("""Attribute 'n_tracers' must be \
+interpretable as an integer. Got: %g""" % (value)) 
+			else: 
+				raise ValueError("""Attribute 'n_tracers' must be positive. \
+Got: %g""" % (value)) 
+		else: 
+			raise TypeError("""Attribute 'n_tracers' must be an integer. \
+Got: %s""" % (type(value))) 
+
+	@property 
+	def verbose(self): 
+		# docstring in python version 
+		return bool(self._mz[0].verbose) 
+
+	@verbose.setter 
+	def verbose(self, value): 
+		""" 
+		Whether or not to print the time as the simulation evolves. 
+
+		Allowed Types 
+		============= 
+		bool 
+
+		Allowed Values 
+		============== 
+		True and False 
+		""" 
+		if isinstance(value, numbers.Number) or isinstance(value, bool): 
+			if value: 
+				self._mz[0].verbose = 1 
+			else: 
+				self._mz[0].verbose = 0 
+		else: 
+			raise TypeError("""Attribute 'verbose' must be interpretable as \
+a boolean. Got: %s""" % (type(value))) 
+
+	@property 
+	def migration(self): 
+		# docstring in python version 
+		return self._migration 
+
+cdef class migration_specifications: 
+
+	""" 
+	Migration specifications for multizone simulations. 
+	""" 
+
+	cdef object _stars 
+	cdef object _gas 
+
+	def __init__(self, int n): 
+		self._stars = migration_matrix(n) 
+		self._gas = migration_matrix(n) 
+
+	@property 
+	def gas(self): 
+		""" 
+		The migration matrix associated with the interstellar gas. 
+
+		Contains user-specified migration prescriptions for use in multizone 
+		simulations. For a multizone simulation with N zones, this is an NxN 
+		matrix. 
+
+		This matrix is defined such that the ij'th element represents the 
+		likelihood that interstellar gas or stars will migrate FROM the i'th 
+		TO the j'th zone in the simulation. These entries may be either 
+		numerical values or functions of time in Gyr. In all cases, the value 
+		at a given time must be between 0 and 1, because the elements are 
+		interpreted as likelihoods. 
+		""" 
+		return self._gas 
+
+	@property 
+	def stars(self): 
+		""" 
+		The migration matrix associated with the stars. 
+
+		Contains user-specified migration prescriptions for use in multizone 
+		simulations. For a multizone simulation with N zones, this is an NxN 
+		matrix. 
+
+		This matrix is defined such that the ij'th element represents the 
+		likelihood that interstellar gas or stars will migrate FROM the i'th 
+		TO the j'th zone in the simulation. These entries may be either 
+		numerical values or functions of time in Gyr. In all cases, the value 
+		at a given time must be between 0 and 1, because the elements are 
+		interpreted as likelihoods. 
+		""" 
+		return self._stars 
 
 
 
