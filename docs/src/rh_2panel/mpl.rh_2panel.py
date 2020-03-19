8561352b3ae5725a127e26605ba1caa25f330cb5
astrobeard <astrobeard21@gmail.com>
2020-03-19 12:27:22 -0400

    documentation expansion started ; readme updates

---
+++ docs/src/rh_2panel/mpl.rh_2panel.py
@@ -0,0 +1,118 @@
+""" 
+Produces the 2-panel figure showing the cumulative return fraction and the 
+main sequence mass fraction for VICE's science documentation. 
+""" 
+
+import matplotlib as mpl 
+import matplotlib.pyplot as plt 
+import numpy as np 
+import vice 
+
+mpl.rcParams["font.family"] = "serif"
+mpl.rcParams["text.usetex"] = True 
+mpl.rcParams["text.latex.preamble"] = [r"\usepackage{amsmath}"]
+mpl.rcParams["errorbar.capsize"] = 5
+mpl.rcParams["axes.linewidth"] = 2
+mpl.rcParams["xtick.major.size"] = 16
+mpl.rcParams["xtick.major.width"] = 2 
+mpl.rcParams["xtick.minor.size"] = 8 
+mpl.rcParams["xtick.minor.width"] = 1 
+mpl.rcParams["ytick.major.size"] = 16
+mpl.rcParams["ytick.major.width"] = 2 
+mpl.rcParams["ytick.minor.size"] = 8 
+mpl.rcParams["ytick.minor.width"] = 1 
+mpl.rcParams["axes.labelsize"] = 30
+mpl.rcParams["xtick.labelsize"] = 25
+mpl.rcParams["ytick.labelsize"] = 25
+mpl.rcParams["legend.fontsize"] = 30
+mpl.rcParams["xtick.direction"] = "in"
+mpl.rcParams["ytick.direction"] = "in"
+mpl.rcParams["ytick.right"] = True
+mpl.rcParams["xtick.top"] = True
+mpl.rcParams["xtick.minor.visible"] = True
+mpl.rcParams["ytick.minor.visible"] = True
+
+
+def setup_axes(): 
+	""" 
+	Sets up a 1x2 subplot figure 
+	""" 
+	plt.clf() 
+	fig = plt.figure(figsize = (14, 7)) 
+	ax1 = fig.add_subplot(121, facecolor = "white") 
+	ax2 = fig.add_subplot(122, facecolor = "white") 
+	ax1.set_ylabel("r") 
+	ax2.set_ylabel("h") 
+	ax1.set_xlabel("Time [Gyr]") 
+	ax2.set_xlabel("Time [Gyr]") 
+	ax1.set_xlim([-1, 11]) 
+	ax2.set_xlim([-1, 11]) 
+	ax1.set_ylim([0.0, 0.5]) 
+	ax2.set_ylim([0.4, 1.0]) 
+	ax1.xaxis.set_ticks([0, 2, 4, 6, 8, 10]) 
+	ax2.xaxis.set_ticks([0, 2, 4, 6, 8, 10]) 
+	return [ax1, ax2] 
+
+
+def plot_r(ax, IMF, color): 
+	""" 
+	Plot the cumulative return fraction 
+
+	ax :: the subplot to plot on 
+	IMF :: the assumed stellar initial mass function 
+	color :: a string denoting the color to plot in 
+	""" 
+	times = np.linspace(0, 10, 1001) 
+	ax.plot(times, 
+		[vice.cumulative_return_fraction(i, IMF = IMF) for i in times], 
+		c = mpl.colors.get_named_colors_mapping()[color]) 
+
+
+def plot_h(ax, IMF, color): 
+	""" 
+	Plot the main sequence mass fraction 
+
+	ax :: the subplot to plot on 
+	IMF :: the assumed stellar initial mass function 
+	color :: a string denoting the color to plot in 
+	""" 
+	times = np.linspace(0, 10, 1001) 
+	ax.plot(times, 
+		[vice.main_sequence_mass_fraction(i, IMF = IMF) for i in times], 
+		c = mpl.colors.get_named_colors_mapping()[color]) 
+	ax.plot(ax.get_xlim(), 
+		2 * [vice.main_sequence_mass_fraction(10 * 8**-3.5, IMF = IMF)], 
+		c = mpl.colors.get_named_colors_mapping()[color],  
+		linestyle = ':') 
+
+
+def legend(ax, IMFs, colors): 
+	""" 
+	Produce the legend 
+
+	ax :: the subplot to put the legend on 
+	IMFs :: the assmed stellar initial mass functions 
+	colors :: the colors each IMF is plotted in 
+	""" 
+	lines = len(IMFs) * [None] 
+	for i in range(len(lines)): 
+		lines[i] = ax.plot([1, 2], [1, 2], label = IMFs[i], 
+			c = mpl.colors.get_named_colors_mapping()["white"])[0] 
+	leg = ax.legend(loc = 4, ncol = 1, frameon = False, 
+		bbox_to_anchor = (0.98, 0.02), handlelength = 0) 
+	for i in range(len(lines)): 
+		lines[i].remove() 
+		leg.get_texts()[i].set_color(colors[i]) 
+
+
+if __name__ == "__main__": 
+	ax1, ax2 = setup_axes() 
+	plot_r(ax1, "Salpeter", "blue") 
+	plot_r(ax1, "Kroupa", "crimson") 
+	plot_h(ax2, "Salpeter", "blue") 
+	plot_h(ax2, "Kroupa", "crimson") 
+	legend(ax1, ["Kroupa", "Salpeter"], ["crimson", "blue"]) 
+	plt.tight_layout() 
+	plt.savefig("rh2panel.pdf") 
+	plt.savefig("rh2panel.png") 
+	plt.clf() 
---
+++ docs/src/timer/mpl.timer.py
@@ -0,0 +1,95 @@
+
+import matplotlib as mpl 
+import matplotlib.pyplot as plt 
+from matplotlib.ticker import FormatStrFormatter as fsf 
+import numpy as np 
+
+mpl.rcParams["font.family"] = "serif"
+mpl.rcParams["text.usetex"] = True 
+mpl.rcParams["text.latex.preamble"] = [r"\usepackage{amsmath}"]
+mpl.rcParams["errorbar.capsize"] = 5
+mpl.rcParams["axes.linewidth"] = 2
+mpl.rcParams["xtick.major.size"] = 16
+mpl.rcParams["xtick.major.width"] = 2 
+mpl.rcParams["xtick.minor.size"] = 8 
+mpl.rcParams["xtick.minor.width"] = 1 
+mpl.rcParams["ytick.major.size"] = 16
+mpl.rcParams["ytick.major.width"] = 2 
+mpl.rcParams["ytick.minor.size"] = 8 
+mpl.rcParams["ytick.minor.width"] = 1 
+mpl.rcParams["axes.labelsize"] = 30
+mpl.rcParams["xtick.labelsize"] = 25
+mpl.rcParams["ytick.labelsize"] = 25
+mpl.rcParams["legend.fontsize"] = 30
+mpl.rcParams["xtick.direction"] = "in"
+mpl.rcParams["ytick.direction"] = "in"
+mpl.rcParams["ytick.right"] = True
+mpl.rcParams["xtick.top"] = True
+mpl.rcParams["xtick.minor.visible"] = True
+mpl.rcParams["ytick.minor.visible"] = True
+
+OUTPUTS = ["timer_5.out", "timer_10.out", "timer_15.out", "timer_20.out", 
+	"timer_25.out"] 
+COLORS = ["red", "gold", "green", "blue", "purple"] 
+N = [5, 10, 15, 20, 25] 
+
+def setup_subplot(): 
+	plt.clf() 
+	fig = plt.figure(figsize = (10, 10)) 
+	ax = fig.add_subplot(111, facecolor = "white") 
+	ax.set_xlabel(r"$\Delta t$ [Gyr]") 
+	ax.set_ylabel(r"$\texttt{VICE}$ Integration Time [seconds]") 
+	ax.set_xscale("log") 
+	ax.set_yscale("log") 
+	ax.tick_params(axis = 'x', which = 'both', pad = 10) 
+	return ax 
+
+def draw(name, ax, color, nelem): 
+	output = np.genfromtxt(name) 
+	x = [i[0] for i in output] 
+	y = [i[1] for i in output] 
+	ax.plot(x, y, c = mpl.colors.get_named_colors_mapping()[color]) 
+	x = np.linspace(1.e-4, 1.e-1, 1000) 
+	fit = best_fit(nelem) 
+	ax.plot(x, list(map(fit, x)), 
+		c = mpl.colors.get_named_colors_mapping()[color], linestyle = ':') 
+
+def legend(ax): 
+	lines = len(OUTPUTS) * [None] 
+	for i in range(len(lines)): 
+		lines[i] = ax.plot([0.001, 0.01], [1, 2], 
+			c = mpl.colors.get_named_colors_mapping()["white"], 
+			label = "%d elements" % (N[i]))[0] 
+	leg = ax.legend(loc = 3, ncol = 1, frameon = False, 
+		bbox_to_anchor = (0.02, 0.02), handlelength = 0) 
+	for i in range(len(lines)): 
+		lines[i].remove() 
+		leg.get_texts()[i].set_color(COLORS[i]) 
+	ax.add_artist(leg) 
+	dummy = ax.plot([0.001, 0.01], [1, 2], 
+		c = mpl.colors.get_named_colors_mapping()["black"], linestyle = ':', 
+		label = r"$N\Delta t^{-2}$ expected fit")[0] 
+	ax.legend(loc = 1, ncol = 1, frameon = False, 
+		bbox_to_anchor = (0.98, 0.98)) 
+	dummy.remove() 
+
+def best_fit(nelem): 
+	return lambda t: 4.2 * nelem * (1.e-3 / t)**2 
+
+if __name__ == "__main__": 
+	ax = setup_subplot() 
+	for i in range(len(N)): 
+		draw(OUTPUTS[i], ax, COLORS[i], N[i]) 
+	legend(ax) 
+	ax.text(0.01, 300, "T = 10 Gyr", fontsize = 25, 
+		color = mpl.colors.get_named_colors_mapping()["black"]) 
+	ax.set_xlim([3.3e-4, 0.06]) 
+	ax.set_ylim([0.025, 1500]) 
+	ax.xaxis.set_major_formatter(fsf("%g")) 
+	ax.yaxis.set_major_formatter(fsf("%g")) 
+	plt.tight_layout() 
+	plt.savefig("timer.pdf") 
+	plt.clf() 
+
+
+
