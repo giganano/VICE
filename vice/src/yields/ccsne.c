c8ee88cd7161399e326f8663cbfa002cf84ff2d1
astrobeard <astrobeard21@gmail.com>
2020-04-14 14:58:33 -0400

    maint: commented-out code removed

--- vice/src/yields/ccsne.c
+++ vice/src/yields/ccsne.c
@@ -17,7 +17,6 @@
 static double interpolate_yield(double m); 
 static double y_cc_numerator(double m); 
 static double y_cc_denominator(double m); 
-// static double get_explodability_fraction(double m); 
 
 
 /* 
@@ -34,76 +33,8 @@ static double y_cc_denominator(double m);
  */
 static double **GRID; 
 static unsigned int GRIDSIZE = 0; 
-// static unsigned int N_MASSES = 0; 
-// static double *MASSES; 
-// static double *EXPLODABILITY; 
 static IMF_ *IMF = NULL; 
 static CALLBACK_1ARG *EXPLODABILITY = NULL; 
-
-
-#if 0 
-/* 
- * Copy the explodability criteria that the user passed to 
- * yields.ccsne.fractional. 
- * 
- * Parameters 
- * ========== 
- * masses: 			The masses themselves. Python will ensure that this is 
- * 					always divisible by two 
- * n_masses: 		The number of masses in the mass binspace 
- * explodability: 	The explosion fractions 
- * 
- * header: ccsne.h 
- */ 
-extern void set_explodability_criteria(double *masses, unsigned int n_masses, 
-	double *explodability) {
-
-	/* Allocate memory, copy the number of masses on the grid */ 
-	N_MASSES = n_masses; 
-	MASSES = (double *) malloc (n_masses * sizeof(double)); 
-	EXPLODABILITY = (double *) malloc ((n_masses - 1) * sizeof(double)); 
-
-	/* 
-	 * Copy over each mass range, and set the explodability equal to 1 in 
-	 * ranges where the user didn't specify an explodability fraction. 
-	 */ 
-	unsigned int i; 
-	for (i = 0; i < n_masses; i++) { 
-		MASSES[i] = masses[i]; 
-	} 
-	for (i = 0; i < n_masses - 1; i++) { /* n_masses always divisible by 2 */ 
-		if (i % 2) { 
-			EXPLODABILITY[i] = 1; 
-		} else { 
-			EXPLODABILITY[i] = explodability[i / 2]; 
-		} 
-	} 
-
-} 
-
-
-/* 
- * Determines the fraction of stars at the stellar mass m in Msun that explode 
- * according to the user's explodability criteria. 
- * 
- * Parameters 
- * ========== 
- * m: 		The stellar mass in Msun 
- * 
- * Returns 
- * ======= 
- * The specified fraction of stars that explode. 
- */ 
-static double get_explodability_fraction(double m) {
-
-	long bin = get_bin_number(MASSES, N_MASSES, m); 
-	if (bin != -1l) {
-		return EXPLODABILITY[bin]; 
-	} else {
-		return 1; 
-	}
-
-} 
 
 
 /* 
@@ -112,9 +43,10 @@ static double get_explodability_fraction(double m) {
  * 
  * Parameters 
  * ========== 
- * intgrl: 		The integral object for the numerator of the yield 
- * imf:			The associated IMF object
- * file:		The nme of the data file containing the grid 
+ * intgrl: 			The integral object for the numerator of the yield 
+ * imf:				The associated IMF object
+ * explodability: 	Stellar explodability as a function of mass 
+ * file:			The nme of the data file containing the grid 
  * 
  * Returns 
  * ======= 
@@ -123,33 +55,6 @@ static double get_explodability_fraction(double m) {
  * 
  * header: ccsne.h 
  */ 
-extern unsigned short IMFintegrated_fractional_yield_numerator(
-	INTEGRAL *intgrl, IMF_ *imf, char *file) {
-
-	/* 
-	 * Initialize these variables globally. This is such that the functions 
-	 * which execute numerical quadrature can accept only one parameter - the 
-	 * stellar mass. 
-	 */ 
-	GRIDSIZE = line_count(file) - header_length(file); 
-	GRID = cc_yield_grid(file); 
-	ADOPTED_IMF = imf; 
-
-	intgrl -> func = &y_cc_numerator; 
-	int x = quad(intgrl); 
-	free(MASSES); 
-	free(EXPLODABILITY); 
-	free(GRID); 
-	intgrl -> func = NULL; 
-	ADOPTED_IMF = NULL; 
-	GRIDSIZE = 0; 
-	N_MASSES = 0; 
-	return x; 
-
-} 
-#endif 
-
-
 extern unsigned short IMFintegrated_fractional_yield_numerator(
 	INTEGRAL *intgrl, IMF_ *imf, CALLBACK_1ARG *explodability, 
 	char *file) { 
@@ -183,30 +88,15 @@ extern unsigned short IMFintegrated_fractional_yield_numerator(
  * Parameters 
  * ========== 
  * intgrl: 		The integral object for the denominator of the yield 
- * imf:			The associated IMF object  
+ * imf:			The associated IMF object 
  * 
  * Returns 
  * ======= 
  * 3 on an unrecognized IMF, otherwise the value returned by quad (see 
  * quadrature.h) 
  * 
- * header: ccsne.h 
+ * header: ccsne.c 
  */ 
-#if 0 
-extern unsigned short IMFintegrated_fractional_yield_denominator(
-	INTEGRAL *intgrl, IMF_ *imf) {
-	
-	ADOPTED_IMF = imf; 
-	intgrl -> func = &y_cc_denominator; 
-	int x = quad(intgrl); 
-	intgrl -> func = NULL; 
-	ADOPTED_IMF = NULL; 
-	return x; 
-
-} 
-#endif 
-
-
 extern unsigned short IMFintegrated_fractional_yield_denominator(
 	INTEGRAL *intgrl, IMF_ *imf) { 
 
@@ -242,7 +132,6 @@ static double interpolate_yield(double m) {
 		for (i = 0; i < GRIDSIZE; i++) {
 			/* if the mass itself is on the grid, just return that yield */ 
 			if (m == GRID[i][0]) {
-				// return explosion_fraction * GRID[i][1]; 
 				return callback_1arg_evaluate(*EXPLODABILITY, m) * GRID[i][1]; 
 			} else { 
 				continue; 
@@ -254,7 +143,6 @@ static double interpolate_yield(double m) {
 		 */
 		for (i = 0; i < GRIDSIZE - 1; i++) {
 			if (GRID[i][0] < m && m < GRID[i + 1][0]) {
-				// return explosion_fraction * interpolate(GRID[i][0], 
 				return callback_1arg_evaluate(*EXPLODABILITY, m) * 
 					interpolate(GRID[i][0], 
 						GRID[i + 1][0], GRID[i][1], GRID[i + 1][1], m); 
@@ -268,7 +156,6 @@ static double interpolate_yield(double m) {
 		 * case, python will raise a warning, and we automatically extrapolate 
 		 * yield linearly from the bottom two elements on the grid. 
 		 */ 
-		// return explosion_fraction * interpolate(GRID[GRIDSIZE - 2][0], 
 		return callback_1arg_evaluate(*EXPLODABILITY, m) * interpolate(
 			GRID[GRIDSIZE - 2][0], 
 			GRID[GRIDSIZE - 1][0], GRID[GRIDSIZE - 2][1], 
@@ -291,7 +178,6 @@ static double interpolate_yield(double m) {
  */ 
 static double y_cc_numerator(double m) { 
 
-	// return interpolate_yield(m) * imf_evaluate(*ADOPTED_IMF, m); 
 	return interpolate_yield(m) * imf_evaluate(*IMF, m); 
 
 } 
@@ -310,7 +196,6 @@ static double y_cc_numerator(double m) {
  */ 
 static double y_cc_denominator(double m) {
 
-	// return m * imf_evaluate(*ADOPTED_IMF, m); 
 	return m * imf_evaluate(*IMF, m); 
 
 } 
