f6487e6dfe984947297f39953ead89b2c28c260e
astrobeard <giganano9@gmail.com>
2020-04-28 16:22:43 -0400

    maint: old code removed from singlezone source test

--- vice/src/singlezone/tests/__init__.py
+++ vice/src/singlezone/tests/__init__.py
@@ -10,15 +10,6 @@ if not __VICE_SETUP__:
 	__all__ = ["test"] 
 	from ....testing import moduletest 
 	from ._singlezone import test 
-	# from . import _singlezone 
-
-	# @moduletest 
-	# def test(): 
-	# 	return ["vice.src.singlezone", 
-	# 		[ 
-	# 			_singlezone.test(run = False) 
-	# 		] 
-	# 	] 
 
 else: 
 	pass 
