c044398f1c1c7cd8e58915ac6f0b886e3b05944f
astrobeard <giganano9@gmail.com>
2021-02-18 12:50:37 -0500

    maint: unnecessary old code removed

--- vice/src/stats.h
+++ vice/src/stats.h
@@ -11,67 +11,6 @@ extern "C" {
 #endif /* PI */ 
 
 /* 
- * Generate a pseudo-random number from a normal distribution. This function 
- * makes use of the Box-Muller tranformation to do so. 
- * 
- * Parameters 
- * ========== 
- * mean: 		The mean of the distribution 
- * sigma: 		The standard deviation of the distribution 
- * 
- * Returns 
- * ======= 
- * A psuedo-random number drawn from a guassian distribution with specified mean 
- * and standard deviation. 
- * 
- * source: stats.c 
- */ 
-extern double normal(double mean, double sigma); 
-
-#if 0 
-/* 
- * Draw a given number of samples from a known distribution. 
- * 
- * Parameters 
- * ========== 
- * dist: 		The distribution itself, assumed to be unnormalized 
- * bins: 		The bin edges on which the distribution is sampled 
- * n_bins: 		The number of bins in the distribution. This is always 1 less 
- * 				than the number of bin edges. 
- * n: 			The number of samples to draw 
- * 
- * Returns 
- * ======= 
- * A double pointer to an n-element array of values drawn from the given 
- * distribution. 
- * 
- * source: stats.c 
- */ 
-extern double *sample(double *dist, double *bins, unsigned long n_bins, 
-	unsigned long n); 
-
-/* 
- * Convert a distribution to a cumulative distribution function (CDF). 
- * 
- * Parameters 
- * ========== 
- * dist: 		The values of the distribution itself in each bin 
- * bins: 		The bin edges on which the distribution is sampled 
- * n_bins: 		The number of bins in the distribution. This is always 1 less 
- * 				than the number of bin edges. 
- * 
- * Returns 
- * ======= 
- * A distribution whose values represent the fraction of values with bin 
- * numbers < N. 
- * 
- * source: stats.c 
- */ 
-extern double *convert_to_CDF(double *dist, double *bins, 
-	unsigned long n_bins); 
-#endif 
-
-/* 
  * Convert a distribution in a given binspace to a probability distribution 
  * function (PDF). 
  * 
--- vice/src/tests/_stats.pxd
+++ vice/src/tests/_stats.pxd
@@ -1,5 +1,4 @@
 # cython: language_level = 3, boundscheck = False 
 
 cdef extern from "stats.h": 
-	unsigned short test_normal() 
 	unsigned short test_convert_to_PDF() 
