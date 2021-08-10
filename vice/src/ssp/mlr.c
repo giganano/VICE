8404f7f797395e133faa638e3f4ed7aa45914d57
astrobeard <giganano9@gmail.com>
2021-08-10 17:47:27 -0400

    [ci skip] dev: Larson (1974) analytic mass-lifetime relation added to vice/src/ssp/mlr.c

--- vice/src/ssp/mlr.c
+++ vice/src/ssp/mlr.c
@@ -67,3 +67,74 @@ extern double main_sequence_turnoff_mass(double t, double postMS) {
 } 
 
 
+/* 
+ * Calculate main sequence turnoff mass according to the analytic form 
+ * presented in Larson (1974), which is a fit to the compilation of 
+ * evolutionary lifetimes presented in Tinsley (1972). 
+ * 
+ * Parameters 
+ * ==========
+ * t: 			Stellar population age in Gyr 
+ * postMS: 		Ratio of a star's main sequence lifetime to its post main 
+ * 				sequence lifetime. 
+ * 
+ * Returns 
+ * =======
+ * The main sequence turnoff mass calculated according to the following 
+ * relation: 
+ * 
+ * log(t) = alpha + (beta + gamma * log(m)) * log(m) 
+ * 
+ * where t is measured in Gyr and m is measured in solar masses. The value of 
+ * alpha quantifies the log of the main sequence lifetime of the sun, which 
+ * sets the overall scaling of this relation. 
+ * 
+ * Notes 
+ * =====
+ * This function takes alpha = 1.0, beta = -3.42, and gamma = 0.88 based on 
+ * Kobayashi (2004), who adopt the form from David, Forman & Jones (1990). 
+ * Kobayashi (2004) uses alpha = 10.0 rather than 1.0, the difference arising 
+ * due to her use of yr rather than Gyr as the time unit. 
+ * 
+ * References 
+ * ==========
+ * David, Forman & Jones (1990), ApJ, 359, 29 
+ * Kobayashi (2004), MNRAS, 347, 74 
+ * Larson (1974), MNRAS, 166, 585 
+ * Tinsley (1972), A&A, 20, 383 
+ * 
+ * header: mlr.h 
+ */ 
+extern double Larson1974(double t, double postMS) {
+
+	if (t) {
+
+		double alpha = log10(SOLAR_LIFETIME); 
+		double beta = -3.42; 
+		double gamma = 0.88; 
+
+		/* 
+		 * Determined via the quadratic formula in the equation above, where 
+		 * the version with the minus sign is the physical answer. If the plus 
+		 * sign is taken from the quadratic, then the turnoff mass increases 
+		 * with time. 
+		 */ 
+
+		double logm = (
+			-beta - sqrt(pow(beta, 2) - 4 * gamma * (alpha - log10(t)))
+		) / (
+			2 * gamma 
+		); 
+
+		return pow(10, logm); 
+
+	} else {
+		#ifdef INFINITY 
+			return INFINITY; 
+		#else 
+			return 500; 
+		#endif 
+	}
+
+}
+
