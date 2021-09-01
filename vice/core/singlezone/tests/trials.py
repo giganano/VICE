0695f9d0af6da8e333e8ffda21c9187c524c233b
astrobeard <giganano9@gmail.com>
2021-09-01 13:27:52 -0500

    maint: trials for various MLR settings added to vice.singlezone trial tests

--- vice/core/singlezone/tests/trials.py
+++ vice/core/singlezone/tests/trials.py
@@ -7,11 +7,20 @@ from __future__ import absolute_import
 __all__ = [
 	"test" 
 ]
+from ...._globals import _VERSION_ERROR_ 
 from ....testing import moduletest 
 from ....testing import unittest 
 from ...outputs import output 
 from ..singlezone import singlezone 
+from ...mlr import mlr 
 import math 
+import sys 
+if sys.version_info[:2] == (2, 7): 
+	strcomp = basestring 
+elif sys.version_info[:2] >= (3, 5): 
+	strcomp = str 
+else: 
+	_VERSION_ERROR_() 
 try: 
 	ModuleNotFoundError 
 except NameError: 
@@ -70,6 +79,62 @@ class generator:
 		return status 
 
 
+class mlr_generator: 
+
+	""" 
+	A callable object designed to run a singlezone object under a given 
+	mass-lifetime relation to ensure that it works properly. 
+	A timestep size of 0.001 is used to ensure that the assumed MLR responds 
+	properly to stellar populations young enough that no stars have died yet. 
+	""" 
+
+	def __init__(self, mlr = "larson1974"): 
+		self.mlr = mlr 
+		self._sz = singlezone(name = "test", elements = ["fe", "sr", "o"], 
+			dt = 0.001) 
+		# stop at 500 Myr, no need to go longer since this already gets into 
+		# the regime where AGB stars will be ejecting yields 
+		self._outtimes = [0.01 * i for i in range(51)] 
+
+	def __call__(self): 
+		try: 
+			current = mlr.setting 
+			mlr.setting = self.mlr 
+			out = self._sz.run(self._outtimes, overwrite = True, 
+				capture = True) 
+			mlr.setting = current 
+		except: 
+			return False 
+		status = True 
+		for i in range(len(out.history["time"])): 
+			status &= not math.isnan(out.history["mass(fe)"][i]) 
+			status &= not math.isnan(out.history["mass(sr)"][i]) 
+			status &= not math.isnan(out.history["mass(o)"][i]) 
+			if not status: break 
+		return status 
+
+	@property 
+	def mlr(self): 
+		r""" 
+		Type : str 
+
+		Default : "larson1974" 
+
+		The MLR setting to test the singlezone object with. 
+		""" 
+		return self._mlr 
+
+	@mlr.setter 
+	def mlr(self, value): 
+		if isinstance(value, strcomp): 
+			if value.lower() in mlr.recognized: 
+				self._mlr = value.lower() 
+			else: 
+				raise ValueError("Unrecognized MLR: %s" % (value)) 
+		else: 
+			raise TypeError("MLR must be of type str. Got: %s" % (type(value))) 
+
+
 @moduletest 
 def test(): 
 	""" 
@@ -100,6 +165,9 @@ def test():
 	for i in _SCHMIDT_: 
 		trials.append(trial("vice.core.singlezone [schmidt :: %s]" % (str(i)), 
 			generator(schmidt = i))) 
+	for i in mlr.recognized: 
+		trials.append(trial("vice.core.singlezone [MLR :: %s]" % (i), 
+			mlr_generator(mlr = i)))  
 	return ["vice.core.singlezone trial tests", trials] 
 
 
