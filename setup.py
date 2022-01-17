a6262eb69968b9a8fa99eacaebd15dd3462ca365
James W. Johnson <43220904+giganano@users.noreply.github.com>
2022-01-17 17:07:51 -0500

    [ci skip] maint: minimum python version 3.6 -> 3.7

--- setup.py
+++ setup.py
@@ -31,8 +31,8 @@ Raises
 	- The name of the extension to reinstall is invalid
 """
 
-# this version requires python >= 3.6.0
-MIN_PYTHON_VERSION = "3.6.0"
+# this version requires python >= 3.7.0
+MIN_PYTHON_VERSION = "3.7.0"
 import sys
 import os
 if sys.version_info[:] < tuple(
@@ -70,7 +70,6 @@ Programming Language :: C
 Programming Language :: Cython
 Programming Language :: Python
 Programming Language :: Python :: 3
-Programming Language :: Python :: 3.6
 Programming Language :: Python :: 3.7
 Programming Language :: Python :: 3.8
 Programming Language :: Python :: 3.9
@@ -376,7 +375,7 @@ def setup_package():
 			"setuptools>=18.0", # automatically handles Cython extensions
 			"Cython>=0.29.0"
 		],
-		python_requires=">=3.6.*, <4",
+		python_requires=">=3.7.*, <4",
 		zip_safe = False,
 		verbose = "-q" not in sys.argv and "--quiet" not in sys.argv
 	)
