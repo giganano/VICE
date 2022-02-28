ac9adbb28ed50b4719d6dc3b4934f036e685ed8e
Liam Dubay <liam.dubay@gmail.com>
2022-02-28 14:42:18 -0500

    patch: fixed path to install dir for Windows

--- vice/_globals.py
+++ vice/_globals.py
@@ -37,7 +37,7 @@ import os
 
 
 # The path to the directory after installation
-_DIRECTORY_ = "%s/" % (os.path.dirname(os.path.abspath(__file__)))
+_DIRECTORY_ = os.path.dirname(os.path.abspath(__file__))
 
 """
 The default bins into which a stellar metallicity distribution function
