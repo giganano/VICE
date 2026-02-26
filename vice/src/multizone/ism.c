e51d8aeffb89424461014ae332c418e914b0fc22
James Johnson <giganano9@gmail.com>
2026-02-26 14:47:35 -0800

    maint: necessary commented out line added back

--- vice/src/multizone/ism.c
+++ vice/src/multizone/ism.c
@@ -88,8 +88,7 @@ extern unsigned short update_zone_evolution(MULTIZONE *mz) {
 				);
 				sz -> ism -> infall_rate = (
 					((*(*sz).ism).mass - (*(*sz).ism).specified[(*sz).timestep]
-						// - mass_recycled[i] - migration_deltas[i]) / (*sz).dt +
-						- mass_recycled[i]) / (*sz).dt +
+						- mass_recycled[i] - migration_deltas[i]) / (*sz).dt +
 					(*(*sz).ism).star_formation_rate + get_outflow_rate(*sz)
 				);
 				break;
@@ -112,8 +111,7 @@ extern unsigned short update_zone_evolution(MULTIZONE *mz) {
 					*(*sz).ism).specified[(*sz).timestep + 1l];
 				double dMg = get_ism_mass_SFRmode(*sz, 0u) - (*(*sz).ism).mass;
 				sz -> ism -> infall_rate = (
-					// (dMg - mass_recycled[i] - migration_deltas[i]) / (*sz).dt +
-					(dMg - mass_recycled[i]) / (*sz).dt +
+					(dMg - mass_recycled[i] - migration_deltas[i]) / (*sz).dt +
 					(*(*sz).ism).star_formation_rate + get_outflow_rate(*sz)
 				);
 				sz -> ism -> mass += dMg;
